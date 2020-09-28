213bc4e33bdd444db4167a6d2d36fafa926d9c05
jazorhe <jazor.he@gmail.com>
2020-09-28 22:52:06 +1000

    the collision update

--- bird-jazor/Bird.lua
+++ bird-jazor/Bird.lua
@@ -1,6 +1,7 @@
 Bird = Class{}
 
 JUMP_VELOCITY = -5
+COLLISION_ALLOWANCE = 2
 
 function Bird:init()
     self.image  = love.graphics.newImage('bird.png')
@@ -25,4 +26,16 @@ end
 
 function Bird:render()
     love.graphics.draw(self.image, self.x, self.y)
+end
+
+function Bird:collides(pipe)
+    if (self.x + COLLISION_ALLOWANCE) + (self.width - COLLISION_ALLOWANCE * 2) >= pipe.x and self.x + COLLISION_ALLOWANCE <= pipe.x + PIPE_WIDTH then
+
+        if (self.y + COLLISION_ALLOWANCE) + (self.height - COLLISION_ALLOWANCE * 2) >= pipe.y and self.y + COLLISION_ALLOWANCE <= pipe.y + PIPE_HEIGHT then
+            return true
+        end
+
+    end
+
+    return false
 end
