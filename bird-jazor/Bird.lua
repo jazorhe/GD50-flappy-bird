70286a39dd1a7ccc53e2fef43a71b87a0078f54d
jazorhe <jazor.he@gmail.com>
2020-09-28 21:15:38 +1000

    add gravity

--- bird-jazor/Bird.lua
+++ bird-jazor/Bird.lua
@@ -7,6 +7,13 @@ function Bird:init()
 
     self.x = VIRTUAL_WIDTH  / 2 - (self.width  / 2)
     self.y = VIRTUAL_HEIGHT / 2 - (self.height / 2)
+
+    self.dy = 0
+end
+
+function Bird:update(dt)
+    self.dy = self.dy + GRAVITY * dt
+    self.y  = self.y  + self.dy
 end
 
 function Bird:render()
