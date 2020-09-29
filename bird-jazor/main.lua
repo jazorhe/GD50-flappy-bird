c77b9ad0e9130cb02006265f7115202a78aefa53
jazorhe <jazor.he@gmail.com>
2020-09-29 21:54:27 +1000

    added todo list

--- bird-jazor/main.lua
+++ bird-jazor/main.lua
@@ -138,3 +138,13 @@ function love.draw()
     love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
     push:finish()
 end
+
+-- Assignment 1 TODO:
+
+-- The Gap Update: Randomize the gap between pipes (vertical space), such that they’re no longer hardcoded to 90 pixels.
+
+-- The Pipe Spawn Update: Randomize the interval at which pairs of pipes spawn, such that they’re no longer always 2 seconds apart.
+
+-- The Medal Update: When a player enters the ScoreState, award them a “medal” via an image displayed along with the score; this can be any image or any type of medal you choose (e.g., ribbons, actual medals, trophies, etc.), so long as each is different and based on the points they scored that life. Choose 3 different ones, as well as the minimum score needed for each one (though make it fair and not too hard to test :)).
+
+-- The Pause Update: Implement a pause feature, such that the user can simply press “P” (or some other key) and pause the state of the game. This pause effect will be slightly fancier than the pause feature we showed in class, though not ultimately that much different. When they pause the game, a simple sound effect should play (I recommend testing out bfxr for this, as seen in Lecture 0!). At the same time this sound effect plays, the music should pause, and once the user presses P again, the gameplay and the music should resume just as they were! To cap it off, display a pause icon in the middle of the screen, nice and large, so as to make it clear the game is paused.
